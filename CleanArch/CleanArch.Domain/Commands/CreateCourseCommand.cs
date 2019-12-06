9b0401fdd997ceeb2bd40f7159287c7afc19d619
Carlos Mac Beath Dominguez <mac.beath@hotmail.com>
2019-12-06 15:42:25 -0500

    Commands,Commandhandlers,Updated Course Repository,Cleaned up IoC Container

---
+++ CleanArch/CleanArch.Domain/Commands/CreateCourseCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArch.Domain.Commands
+{
+    public class CreateCourseCommand:CourseCommand
+    {
+        public CreateCourseCommand(
+            string name,
+            string description,
+            string imageurl)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageurl;
+        }
+    }
+}
